b7df7e9532a1ece97b37f1bfb39153592f71df42
MilkySin <blannkisblank@gmail.com>
2022-10-16 00:08:05 +0700

    better logic

--- Copium/square.py
+++ Copium/square.py
@@ -4,16 +4,18 @@ import numpy as np
 def SquareArray(row, col):
     listf = [i for i in range(1, row * col + 1)]
     square = np.array(listf).reshape(row, col)
-    print(square)
     total = 1
-    total1 = 1
-    for i in square:
-        for j in i:
-            if (j - 1) % (row + 1) == 0:
-                total *= j
-            if (j - 1) % (row - 1) == 0 and j - 1 != 0 and j != row * col:
-                total1 *= j
-    return total, total1
+    total2 = 1
+    print(square)
+    for i in range(row):
+        total *= square[i][i]
+        total2 *= square[i][row - i - 1]
+        # for j in i:
+        #     if (j - 1) % (row + 1) == 0:
+        #         total *= j
+        #     if (j - 1) % (row - 1) == 0 and j - 1 != 0 and j != row * col:
+        #         total1 *= j
+    return total, total2
 
 
 def main():
